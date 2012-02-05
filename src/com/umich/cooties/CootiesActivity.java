febffec0f4504fd40ce8bb91f5c41e2edacf026c
oshikryu <oshikryu@github.com>
2012-02-04 21:25:58 -0500

    Added a database for PERSON, still need a database for Relationships.

--- src/com/umich/cooties/CootiesActivity.java
+++ src/com/umich/cooties/CootiesActivity.java
@@ -1,13 +1,115 @@
 package com.umich.cooties;
 
 import android.app.Activity;
+import com.umich.cooties.SQLiteAdapter;
+
+import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class CootiesActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	 EditText inputContent1, inputContent2;
+	 Button buttonAdd, buttonDeleteAll, buttonViewData, buttonViewForm;
+	 
+	 private SQLiteAdapter mySQLiteAdapter;
+	 ListView listContent;
+
+	 SimpleCursorAdapter cursorAdapter;
+	 Cursor cursor;
+
+	   /** Called when the activity is first created. */
+
+	   @Override
+
+	   public void onCreate(Bundle savedInstanceState) {
+	       super.onCreate(savedInstanceState);
+	       setUpForm();
+	   }
+	   
+	   public void setUpForm() {
+		   setContentView(R.layout.main);
+	       inputContent1 = (EditText)findViewById(R.id.content1);
+	       inputContent2 = (EditText)findViewById(R.id.content2);
+	       buttonAdd = (Button)findViewById(R.id.add);
+	       buttonDeleteAll = (Button)findViewById(R.id.deleteall);
+	       buttonViewData = (Button)findViewById(R.id.viewdata);
+
+	       mySQLiteAdapter = new SQLiteAdapter(this);
+	       mySQLiteAdapter.openToWrite();
+	       cursor = mySQLiteAdapter.queueAll();
+
+	       buttonAdd.setOnClickListener(buttonAddOnClickListener);
+	       buttonDeleteAll.setOnClickListener(buttonDeleteAllOnClickListener);
+	       buttonViewData.setOnClickListener(buttonViewDataOnClickListener);
+	   }
+
+	   Button.OnClickListener buttonAddOnClickListener = new Button.OnClickListener(){
+
+	  @Override
+
+	  public void onClick(View arg0) {
+	   // TODO Auto-generated method stub
+	   String data1 = inputContent1.getText().toString();
+	   String data2 = inputContent2.getText().toString();
+	   mySQLiteAdapter.insert(data1, data2);
+	  	  } 
+
+	   };
+
+	   Button.OnClickListener buttonDeleteAllOnClickListener  = new Button.OnClickListener(){
+	  @Override
+
+		  public void onClick(View arg0) {
+		   // TODO Auto-generated method stub
+		   mySQLiteAdapter.deleteAll();
+		  }
+
+	   };
+
+	   Button.OnClickListener buttonViewDataOnClickListener  = new Button.OnClickListener(){
+	  @Override
+
+		  public void onClick(View arg0) {
+		   // TODO Auto-generated method stub
+		  setContentView(R.layout.dataview);
+		  listContent = (ListView)findViewById(R.id.contentlist);
+	      String[] from = new String[]{SQLiteAdapter.KEY_ID, SQLiteAdapter.KEY_CONTENT1, SQLiteAdapter.KEY_CONTENT2};
+	      int[] to = new int[]{R.id.id, R.id.text1, R.id.text2};
+	      cursorAdapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.row, cursor, from, to);
+	      listContent.setAdapter(cursorAdapter);
+		  updateList();
+	      buttonViewForm = (Button)findViewById(R.id.viewform);
+		  buttonViewForm.setOnClickListener(buttonViewFormOnClickListener);
+		  }
+
+	   };
+	   
+	   Button.OnClickListener buttonViewFormOnClickListener  = new Button.OnClickListener(){
+	  @Override
+
+		  public void onClick(View arg0) {
+		   // TODO Auto-generated method stub
+		  setUpForm();
+		  }
+
+	   };
+	   
+	 @Override
+
+	 protected void onDestroy() {
+	  // TODO Auto-generated method stub
+	  super.onDestroy();
+	  mySQLiteAdapter.close();
+	 }
+
+
+	 private void updateList(){
+	  cursor.requery();
+	   }
 }
